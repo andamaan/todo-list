405305d602db51242968b548832f7658889538ef
Andamann <marian.molina@outlook.fr>
2019-10-11 19:22:20 +0200

    build

---
+++ build/precache-manifest.98eb726de1530c2aa8f34bca13db78c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d7b71a676c50f73752a9f5300e87a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30671d050ae6ffab6759",
+    "url": "/static/css/main.3498ef16.chunk.css"
+  },
+  {
+    "revision": "9f44541eaacf787f8535",
+    "url": "/static/js/2.3eab878f.chunk.js"
+  },
+  {
+    "revision": "30671d050ae6ffab6759",
+    "url": "/static/js/main.04b1809c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9930d42a423be96617d99965eec44995",
+    "url": "/static/media/delete.9930d42a.svg"
+  },
+  {
+    "revision": "1275100c4c2f4aee0a9546f98e0532ae",
+    "url": "/static/media/women.1275100c.svg"
+  }
+]);
